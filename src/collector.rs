cf3c22a4ed034c90037c4c7697669164372c24a9
Fox_white <vct.xie@gmail.com>
2023-09-25 15:44:19 +0800

    :sparkles: better code style

--- src/collector.rs
+++ src/collector.rs
@@ -1,25 +1,19 @@
-use crate::Crawler;
 use crate::{Context, PageType};
+use crate::{Crawler, Respond};
 use reqwest::{Client, Response};
-use scraper::{ElementRef, Html, Selector};
+use scraper::{ElementRef, Selector};
+use std::fmt::Display;
 use std::future::Future;
-use std::pin::Pin;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock, RwLockReadGuard};
 use std::time::Duration;
 use tokio::time::sleep;
 use url::Url;
 
-type SelectorHandler<T> = Box<
-    dyn Fn(Arc<RwLock<Context<T>>>, Arc<Crawler<T>>, ElementRef)
-        + Sync
-        + Send,
->;
-type ResponseHandler<T> = Box<
-    dyn Fn(&mut Context<T>, Arc<Crawler<T>>, Response) -> Pin<Box<dyn Future<Output = ()>>>
-        + Sync
-        + Send,
->;
+type SelectorHandler<T> =
+    Box<dyn Fn(Arc<Context<T>>, Arc<Crawler<T>>, ElementRef) + Sync + Send>;
+type ResponseHandler<T> =
+    Box<dyn Fn(Arc<Context<T>>, Arc<Crawler<T>>, Arc<Respond>) + Sync + Send>;
 pub type SelectorHandlerPairs<T> = Vec<(Selector, SelectorHandler<T>)>;
 pub type ResponseHandlers<T> = Vec<ResponseHandler<T>>;
 
@@ -48,59 +42,63 @@ impl<T: Sync + Send + 'static> Collector<T> {
 }
 
 #[derive(Default)]
-pub struct HandlerAdder<T: Sync + Send> {
-    selector_handler: SelectorHandlerPairs<T>,
-    response_handler: ResponseHandlers<T>,
+pub struct Handlers<T: Sync + Send> {
+    pub(crate) selector_handler: SelectorHandlerPairs<T>,
+    pub(crate) response_handler: ResponseHandlers<T>,
 }
 
-impl<T: Sync + Send> HandlerAdder<T> {
-    pub fn new() -> HandlerAdder<T> {
-        HandlerAdder {
+impl<T: Sync + Send + 'static> Handlers<T> {
+    pub fn new() -> Handlers<T> {
+        Handlers {
             selector_handler: SelectorHandlerPairs::default(),
             response_handler: ResponseHandlers::default(),
         }
     }
 
-    fn take(self) -> SelectorHandlerPairs<T> {
-        self.selector_handler
-    }
-
-    async fn _selector_wrapper<'a, F, Fut>(f: F, ctx: &mut Context<T>, crawler: Arc<Crawler<T>>, sel: String)
+    pub fn on_selector<F1, F2, Fut, K, S>(
+        &mut self,
+        selector: S,
+        preprocessor: F1,
+        processor: F2,
+    ) -> &mut Self
     where
-        F: Fn(&mut Context<T>, Arc<Crawler<T>>, ElementRef) -> Fut + 'static + Sync + Send,
-        Fut: Future<Output = ()> + 'static + Sync + Send
-    {
-        let html = Html::parse_fragment(sel.as_str());
-        f(ctx,crawler,html.root_element()).await;
-    }
-
-    pub fn on_selector<F>(&mut self, selector: &str, f: F) -> &mut Self
-    where
-        F: Fn(Arc<RwLock<Context<T>>>, Arc<Crawler<T>>, ElementRef) + 'static + Sync + Send,
+        F1: Fn(Arc<Context<T>>, ElementRef) -> Option<K> + 'static + Sync + Send,
+        F2: Fn(Arc<Context<T>>, Arc<Crawler<T>>, K) -> Fut + 'static + Sync + Send,
+        Fut: Future<Output = ()> + 'static + Send,
+        K: 'static + Send,
+        S: AsRef<str> + Display,
     {
         self.selector_handler.push((
-            Selector::parse(selector)
+            Selector::parse(selector.as_ref())
                 .unwrap_or_else(|_| panic!("Cannot Parse Selector {}", selector)),
-            Box::new(f),
+            Box::new(move |a, b, c| {
+                let k = preprocessor(a.clone(), c);
+                k.map(|k| processor(a, b, k))
+                    .map(|future| tokio::spawn(future));
+            }),
         ));
         self
     }
 
-    pub fn on_response<F, Fut>(&mut self, f: F) -> &mut Self
+    pub fn on_response<F1, F2, Fut, K>(&mut self, preprocessor: F1, processor: F2) -> &mut Self
     where
-        F: Fn(&mut Context<T>, Arc<Crawler<T>>, Response) -> Fut + 'static + Sync + Send,
-        Fut: Future<Output = ()> + 'static + Sync + Send,
+        F1: Fn(Arc<Context<T>>, Arc<Respond>) -> Option<K> + 'static + Sync + Send,
+        F2: Fn(Arc<Context<T>>, Arc<Crawler<T>>, K) -> Fut + 'static + Sync + Send,
+        Fut: Future<Output = ()> + 'static + Send,
+        K: 'static + Send,
     {
         self.response_handler
             .push(Box::new(move |ctx, crawler, resp| {
-                Box::pin(f(ctx, crawler, resp))
+                let k = preprocessor(ctx.clone(), resp);
+                k.map(|k| processor(ctx, crawler, k))
+                    .map(|future| tokio::spawn(future));
             }));
         self
     }
 }
 
 pub trait Scraper: Sized + Sync + Send + Default + 'static {
-    fn get_handlers(adder: &mut HandlerAdder<Self>);
+    fn get_handlers(adder: &mut Handlers<Self>);
 
     fn thread_count() -> usize {
         16
@@ -111,12 +109,13 @@ pub trait Scraper: Sized + Sync + Send + Default + 'static {
     }
 
     fn build_collector() -> Option<Collector<Self>> {
-        let mut constructor = HandlerAdder::new();
+        let mut constructor = Handlers::new();
         Self::get_handlers(&mut constructor);
-        let handler = Arc::new(constructor.take());
+        let handlers = Arc::new(constructor);
+
         let crawler = Arc::new(Crawler::new(
             Self::thread_count(),
-            handler,
+            handlers,
             Arc::new(Self::default()),
             Arc::new(
                 Client::builder()
--- src/crawler.rs
+++ src/crawler.rs
@@ -1,11 +1,32 @@
-use crate::SelectorHandlerPairs;
+use crate::{Handlers, ResponseHandlers, SelectorHandlerPairs};
 use backoff::ExponentialBackoff;
-use log::{error, info, warn};
-use reqwest::Client;
+use log::{error, warn};
+use reqwest::header::HeaderMap;
+use reqwest::{Client, Response, StatusCode};
 use scraper::Html;
 use std::sync::{Arc, RwLock};
-use tokio::sync::{Semaphore};
+use tokio::sync::Semaphore;
 use url::Url;
+
+pub struct Respond {
+    pub url: Url,
+    pub content_length: Option<u64>,
+    pub header: HeaderMap,
+    pub status: StatusCode,
+    pub text: String,
+}
+
+impl Respond {
+    async fn from(value: Response) -> Option<Self> {
+        Some(Self {
+            url: value.url().clone(),
+            content_length: value.content_length().take(),
+            header: value.headers().clone(),
+            status: value.status(),
+            text: value.text().await.ok()?,
+        })
+    }
+}
 
 pub struct NewsData {}
 
@@ -29,7 +50,7 @@ pub struct Context<T: Sync + Send> {
 pub struct Crawler<T: Sync + Send> {
     pub(crate) semaphore: Arc<Semaphore>,
     pub(crate) max_permit: usize,
-    handlers: Arc<SelectorHandlerPairs<T>>,
+    handlers: Arc<Handlers<T>>,
     custom_state: Arc<T>,
     client: Arc<Client>,
 }
@@ -37,7 +58,7 @@ pub struct Crawler<T: Sync + Send> {
 impl<T: 'static + Sync + Send> Crawler<T> {
     pub fn new(
         max_permit: usize,
-        handlers: Arc<SelectorHandlerPairs<T>>,
+        handlers: Arc<Handlers<T>>,
         custom_state: Arc<T>,
         client: Arc<Client>,
     ) -> Crawler<T> {
@@ -52,11 +73,18 @@ impl<T: 'static + Sync + Send> Crawler<T> {
 
     pub async fn visit(self: Arc<Self>, url: Url, page_type: PageType) {
         let semaphore = self.semaphore.clone();
-        let handlers = self.handlers.clone();
         let custom_state = self.custom_state.clone();
         let client = self.client.clone();
-        self.visit_worker(url, page_type, semaphore, handlers, custom_state, client)
-            .await
+        let handlers = self.handlers.clone();
+        self.visit_worker(
+            url,
+            page_type,
+            semaphore,
+            handlers,
+            custom_state,
+            client,
+        )
+        .await
     }
 
     async fn visit_worker(
@@ -64,12 +92,11 @@ impl<T: 'static + Sync + Send> Crawler<T> {
         url: Url,
         page_type: PageType,
         semaphore: Arc<Semaphore>,
-        handlers: Arc<SelectorHandlerPairs<T>>,
+        handlers: Arc<Handlers<T>>,
         custom_state: Arc<T>,
         client: Arc<Client>,
     ) {
-        let permit = semaphore.acquire_owned().await.unwrap();
-        info!("Visiting {}", url);
+        let _permit = semaphore.acquire().await.unwrap();
         let url_str = url.as_str();
         match backoff::future::retry(ExponentialBackoff::default(), || async {
             Ok(client.get(url_str).send().await?)
@@ -77,29 +104,33 @@ impl<T: 'static + Sync + Send> Crawler<T> {
         .await
         {
             Ok(k) => {
-                let url = k.url().clone();
-                let Ok(text) = k.text().await else {
+                let resp = Respond::from(k).await;
+                let Some(respond) = resp else {
                     warn!("Invalid Content type at {}", url_str);
-                    drop(permit);
                     return
                 };
-                let fragment = Html::parse_document(text.as_str());
-                let ctx = Arc::new(RwLock::new(Context {
+                let respond = Arc::new(respond);
+                let fragment = Html::parse_document(respond.text.as_str());
+                let ctx = Arc::new(Context {
                     state: custom_state,
                     page_type,
-                    current_address: url,
-                }));
-                handlers
+                    current_address: respond.url.clone(),
+                });
+
+                handlers.response_handler
+                    .iter()
+                    .for_each(|handler| handler(ctx.clone(), self.clone(), respond.clone()));
+
+                handlers.selector_handler
                     .iter()
                     .map(|(sel, handler)| (fragment.select(sel), handler))
-                    .for_each(|(htmls, handler)| {
-                        htmls.for_each(|html| handler(ctx.clone(), self.clone(), html))
+                    .for_each(|(select, handler)| {
+                        select.for_each(|element| handler(ctx.clone(), self.clone(), element))
                     });
             }
             Err(err) => {
                 error!("{}", err)
             }
         };
-        drop(permit);
     }
 }
